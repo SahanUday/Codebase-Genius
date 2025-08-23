f9154866a153d8e07f9793fee61504039f27f72a
Sahan Udayanga <136253767+SahanUday@users.noreply.github.com>
2025-08-23 12:09:52 +0530

    add getting_relationship function

---
+++ test.py
@@ -0,0 +1,16 @@
+class Rels:
+    def __init__(self, from_abstraction: str, to_abstraction: str, label: str):
+        self.from_abstraction = from_abstraction
+        self.to_abstraction = to_abstraction
+        self.label = label
+
+rels_list = [
+    Rels(from_abstraction='GeofencingEngine', to_abstraction='FirebaseACCommander', label='sends AC commands'),
+    Rels(from_abstraction='ScheduleTimerManager', to_abstraction='FirebaseACCommander', label='sends AC commands'),
+    Rels(from_abstraction='SmartControlAgent', to_abstraction='FirebaseACCommander', label='sends AC commands'),
+    Rels(from_abstraction='SmartControlAgent', to_abstraction='SensorDataStreamer', label='receives sensor data')
+]
+
+# access the first element's label
+first_label = rels_list[0].label
+print(first_label) 
