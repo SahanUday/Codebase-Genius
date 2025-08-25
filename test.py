e450122151c8b6e28439b37bb292cb140adda02a
Sahan Udayanga <136253767+SahanUday@users.noreply.github.com>
2025-08-25 20:00:40 +0530

    update

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-class Rels:
-    def __init__(self, from_abstraction: str, to_abstraction: str, label: str):
-        self.from_abstraction = from_abstraction
-        self.to_abstraction = to_abstraction
-        self.label = label
+l1 =[1,2,3,4]
+l2 = [5,1,3]
+l3 = [9,10,11,12]
 
-rels_list = [
-    Rels(from_abstraction='GeofencingEngine', to_abstraction='FirebaseACCommander', label='sends AC commands'),
-    Rels(from_abstraction='ScheduleTimerManager', to_abstraction='FirebaseACCommander', label='sends AC commands'),
-    Rels(from_abstraction='SmartControlAgent', to_abstraction='FirebaseACCommander', label='sends AC commands'),
-    Rels(from_abstraction='SmartControlAgent', to_abstraction='SensorDataStreamer', label='receives sensor data')
-]
-
-# access the first element's label
-a= rels_list[0]
-print(rels_list.index(a))
-
+print(l1+l2)
